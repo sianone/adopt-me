234d332d17e8c40905b228cfc2587e62503232bc
sianone <suelenianone@hotmail.com>
2021-09-03 05:25:01 -0300

    Add strict mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
-
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams/>
+      <SearchParams />
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
