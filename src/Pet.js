1d10e01b9ec8ba2416939fdb64f26b7c6f299c5f
sianone <suelenianone@hotmail.com>
2021-09-03 04:34:17 -0300

    Fix pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,16 +1,18 @@
 const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
+
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-  if (props.images.length) {
-    hero = props.images[0];
+  if (images.length) {
+    hero = images[0];
   }
   return (
-    <a href={`details/${props.id}`} className="pet">
+    <a href={`/details/${id}`} className="pet">
       <div className="image-container">
-        <img src={hero} alt={props.name} />
+        <img src={hero} alt={name} />
       </div>
-      <div>
-        <h1>{props.name}</h1>
-        <h2> {`${props.animal} - ${props.breed} - ${location}`}</h2>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2> {`${animal} - ${breed} - ${location}`}</h2>
       </div>
     </a>
   );
