5152555d6f9fa3f53fd72dada57ac86b27e2f9b3
sianone <suelenianone@hotmail.com>
2021-09-10 11:26:41 -0300

    Add carousel interaction. Also, this binding

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,6 +9,13 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (event) => {
+    // arrow functions binds this. could also use bind() onClick jsx atribute
+    this.setState({
+      active: +event.target.dataset.index, //coerce string to number with + sign
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -21,6 +28,8 @@ class Carousel extends Component {
             <img
               key={photo}
               src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
